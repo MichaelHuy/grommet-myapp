5a400073efb95ba8b33d9aeaddc2efd52e9dc3cd
HuyLe <lehuydit@gmail.com>
2019-04-18 15:47:14 +0700

    grommet v2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import logo from './logo.svg';
 // import './App.css';
 
+import { Box, Button, Collapsible, Heading, Grommet, ResponsiveContext } from 'grommet'
+import { Notification } from 'grommet-icons'
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+      noxcolor: '#A8001C'
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    }
+  }
+}
+const AppBar = props => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='noxcolor'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+)
+
 class App extends Component {
-  render() {
+  state = {
+    showSidebar: false
+  }
+  render () {
+    const { showSidebar } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Grommet theme={theme} full>
+        <Box fill>
+          <AppBar>
+            <Heading level='3' margin='none'>
+              My App
+            </Heading>
+            <Button
+              icon={<Notification />}
+              onClick={() => this.setState(prevState => ({ showSidebar: !prevState.showSidebar }))}
+            />
+          </AppBar>
+          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+            <Box flex align='center' justify='center'>
+              app body
+            </Box>
+            {/* <Box width='medium' background='light-2' elevation='small' align='center' justify='center'>
+              sidebar
+            </Box> */}
+
+            <Collapsible direction='horizontal' open={showSidebar}>
+              <Box flex width='medium' background='light-2' elevation='small' align='center' justify='center'>
+                sidebar
+              </Box>
+            </Collapsible>
+          </Box>
+        </Box>
+      </Grommet>
+    )
   }
 }
 
-export default App;
+export default App
